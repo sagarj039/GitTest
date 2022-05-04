850032b52e8609344cb182f94eddb1d3ccd57501
sagarj039 <sagarj039@gmail.com>
2022-05-04 16:56:04 +0530

    Added Addition Program File

---
+++ Add.java
@@ -0,0 +1,26 @@
+package basicPrograms;
+
+public class AddProg1 {
+	public static int div()
+	{
+		int num = 23;
+		int num1 = 5;
+		int num2 = 10;
+		
+		if (num%3==0)
+		{
+			return num + num1;
+		}
+		else
+		{
+			return num + num2;
+		}
+}
+
+	public static void main(String[] args) {
+		System.out.println("a=" + div());
+		System.out.println("This is addition program");
+		System.out.println("Thank you");
+		
+}
+}
